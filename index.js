5a54d5579540d1e679a1721cf16f05bcefe9da82
yogle <65646900+tinokim@users.noreply.github.com>
2021-03-26 02:51:58 +0900

    1#

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require("mongoose")
+mongoose.connect('mongodb+srv://kimtino:1234@boilerplate.ep46n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+ {useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
+}).then(()=>console.log('MongoDB Connected..'))
+.catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hi')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})  
